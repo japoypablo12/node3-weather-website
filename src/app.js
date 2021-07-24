ee867382278c849047113305d64600727436cb1a
John Paul Pablo <japoypablo12@gmail.com>
2021-07-24 14:24:29 +0800

    Init commit

---
+++ src/app.js
@@ -0,0 +1,107 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geoCode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+const app = express();
+
+//
+// Directory of file need to call, DEFINE PATHS FOR EXPRESS CONFIG
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// SETUP HANDLEBARS ENGINE AND VIEWS LOCATION
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// SETUP STATIC DIRECTORY TO SERVE
+app.use(express.static(publicDirectoryPath));
+
+// HOME PAGE
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "John Paul Pablo",
+  });
+});
+
+// ABOUT PAGE
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "John Paul Pablo",
+  });
+});
+
+// HELP PAGE
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    message: "If you need help, I'll help you",
+    name: "John Paul Pablo",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide a address",
+    });
+  }
+  geoCode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      } else {
+        // CALLBACK CHAINING
+        forecast(latitude, longitude, (error, forecastData) => {
+          if (error) {
+            res.send({ error });
+          }
+          res.send({
+            location: location,
+            forecastData: forecastData,
+            address: req.query.address,
+          });
+        });
+      }
+    }
+  );
+});
+
+// TESTING
+// app.get("/products", (req, res) => {
+//   if (!req.query.search) {
+//     return res.send({
+//       error: "You must provide a search term",
+//     });
+//   }
+//   console.log(req.query.search);
+//   res.send({
+//     products: [],
+//   });
+// });
+
+app.get("/help/*", (req, res) => {
+  res.render("404pages", {
+    title: "404pages",
+    name: "Andrew Mead",
+    errorMessage: "Help article not found",
+  });
+});
+// 404 Pages
+app.get("*", (req, res) => {
+  res.render("404pages", {
+    title: "404pages",
+    name: "Andrew Mead",
+    errorMessage: "Page not found",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up port 3000.");
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,36 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=bcb71105f581dfe7e485b0a109662eba&query=${latitude},${longitude}&units=f`;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service");
+    } else if (body.success === false) {
+      callback("Unable to find location");
+    } else {
+      callback(
+        undefined,
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out, but it feels like ${body.current.feelslike} degrees`
+      );
+    }
+  });
+};
+
+// const url =
+//   "http://api.weatherstack.com/current?access_key=bcb71105f581dfe7e485b0a109662eba&query=37.8267,-122.4233&units=f";
+
+// // requesting data from weatherstack
+// request({ url: url, json: true }, (error, response) => {
+//   if (error) {
+//     console.log("Unable to connect to weather service");
+//   } else if (response.body.error) {
+//     console.log("Unable to find location");
+//   } else {
+//     console.log(
+//       `${response.body.current.weather_descriptions[0]}.It is currently ${response.body.current.temperature} degrees out, but it feels like ${response.body.current.feelslike} degrees`
+//     );
+//   }
+// });
+
+module.exports = forecast;
